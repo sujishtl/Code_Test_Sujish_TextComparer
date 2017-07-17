66f2e11691443af30058a75d810359f9630d3cc8
sujishtl <sujishtl@gmail.com>
2017-07-17 17:39:35 +1000

    Add files via upload

--- TextComparer.Test/TextComparerTests.cs
+++ TextComparer.Test/TextComparerTests.cs
@@ -1,214 +1,189 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Code_Test_Sujish_TextComparer;
 using System.Collections.Generic;
+using System.Linq;
 // Have used Roy Osherove’s Naming convention
 namespace TextComparer.Test
 {
     [TestClass]
     public class TextComparerTests
     {
+        #region FunctionCompareusingLegacyMethod
         [TestMethod]
         public void LegacyComparison_Strings_ReturnsMatchingPositions()
         {
             //Arrange
-            List<int> expected = new List<int>();
-            expected.Add(1);
-            expected.Add(26);
-            expected.Add(51);
+            var expected = new List<int> {1, 26, 51};
+            ICompare comparer = new LegacyComparer();
+
             //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual=comparer.LegacyComparison("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "polly");
+            var actual=comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "polly");
 
             //Assert
-
-            CollectionAssert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.SequenceEqual(actual));
         }
         [TestMethod]
         public void LegacyComparison_CaseSensitiveStrings_ReturnsMatchingPositions()
         {
             //Arrange
-            List<int> expected = new List<int>();
-            expected.Add(1);
-            expected.Add(26);
-            expected.Add(51);
-            //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual = comparer.LegacyComparison("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Polly");
+            var expected = new List<int> {1, 26, 51};
+            ICompare comparer = new LegacyComparer();
+
+            //Act        
+            var actual = comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Polly");
 
             //Assert
 
-            CollectionAssert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.SequenceEqual(actual));
         }
 
         [TestMethod]
         public void LegacyComparison_TwoCharSubtextStrings_ReturnsMatchingPositions()
         {
             //Arrange
-            List<int> expected = new List<int>();
-            expected.Add(3);
-            expected.Add(28);
-            expected.Add(53);
-            expected.Add(78);
-            expected.Add(82);
+            var expected = new List<int> {3, 28, 53, 78, 82};
+            ICompare comparer = new LegacyComparer();
 
             //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual = comparer.LegacyComparison("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "ll");
+            
+            var actual = comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "ll");
 
             //Assert
 
-            CollectionAssert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.SequenceEqual(actual));
         }
 
         [TestMethod]
         public void LegacyComparison_TwoCharCaseSensitiveStrings_ReturnsMatchingPositions()
         {
             //Arrange
-            List<int> expected = new List<int>();
-            expected.Add(3);
-            expected.Add(28);
-            expected.Add(53);
-            expected.Add(78);
-            expected.Add(82);
+            var expected = new List<int> {3, 28, 53, 78, 82};
+            ICompare comparer = new LegacyComparer();
             //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual = comparer.LegacyComparison("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Ll");
+            var actual = comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Ll");
 
             //Assert
 
-            CollectionAssert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.SequenceEqual(actual));
         }
 
         [TestMethod]
         public void LegacyComparison_NotMatchingStrings_ReturnsNothing()
         {
             //Arrange
-            List<int> expected = new List<int>();
+            var expected = new List<int>();
+            ICompare comparer = new LegacyComparer();
             //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual = comparer.LegacyComparison("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "X");
+  
+            var actual = comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "X");
 
             //Assert
 
-            CollectionAssert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.SequenceEqual(actual));
         }
 
         [TestMethod]
         public void LegacyComparison_NotMatchingStringswithSingleChar_ReturnsNothing()
         {
             //Arrange
-            List<int> expected = new List<int>();       
+            var expected = new List<int>();
+            ICompare comparer = new LegacyComparer();
             //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual = comparer.LegacyComparison("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Polx");
+            var actual = comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Polx");
 
             //Assert
 
-            CollectionAssert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.SequenceEqual(actual));
         }
+        #endregion
 
-        #region FunctionCompareWithField
+        #region FunctionCompareWithYield
         [TestMethod]
         public void CompareWithYield_Strings_ReturnsMatchingPositions()
         {
             //Arrange
-            List<int> expected = new List<int>();
-            expected.Add(1);
-            expected.Add(26);
-            expected.Add(51);
+            var expected = new List<int> {1, 26, 51};
+            ICompare comparer = new YieldComparer();
             //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual = comparer.CompareWithYield("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "polly");
+            var actual = comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "polly");
 
             //Assert
 
-            CollectionAssert.AreEqual(expected, actual);
+            //CollectionAssert.AreEqual(expected, actual);
             Assert.IsTrue(expected.SequenceEqual(actual));
+      
 
         }
         [TestMethod]
         public void CompareWithYield_CaseSensitiveStrings_ReturnsMatchingPositions()
         {
             //Arrange
-            List<int> expected = new List<int>();
-            expected.Add(1);
-            expected.Add(26);
-            expected.Add(51);
+            var expected = new List<int> {1, 26, 51};
+            ICompare comparer = new YieldComparer();
             //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual = comparer.CompareWithYield("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Polly");
+            var actual = comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Polly");
 
             //Assert
 
-            CollectionAssert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.SequenceEqual(actual));
         }
 
         [TestMethod]
         public void CompareWithYield_TwoCharSubtextStrings_ReturnsMatchingPositions()
         {
             //Arrange
-            List<int> expected = new List<int>();
-            expected.Add(3);
-            expected.Add(28);
-            expected.Add(53);
-            expected.Add(78);
-            expected.Add(82);
+            var expected = new List<int> {3, 28, 53, 78, 82};
+            ICompare comparer = new YieldComparer();
 
             //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual = comparer.CompareWithYield("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "ll");
+            var actual = comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "ll");
 
             //Assert
 
-            CollectionAssert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.SequenceEqual(actual));
         }
 
         [TestMethod]
         public void CompareWithYield_TwoCharCaseSensitiveStrings_ReturnsMatchingPositions()
         {
             //Arrange
-            List<int> expected = new List<int>();
-            expected.Add(3);
-            expected.Add(28);
-            expected.Add(53);
-            expected.Add(78);
-            expected.Add(82);
+            var expected = new List<int> {3, 28, 53, 78, 82};
+            ICompare comparer = new YieldComparer();
             //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual = comparer.CompareWithYield("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Ll");
+            var actual = comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Ll");
 
             //Assert
 
-            CollectionAssert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.SequenceEqual(actual));
         }
 
         [TestMethod]
         public void CompareWithYield_NotMatchingStrings_ReturnsNothing()
         {
             //Arrange
-            List<int> expected = new List<int>();
-            //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual = comparer.CompareWithYield("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "X");
+            var expected = new List<int>();
+            ICompare comparer = new YieldComparer();
+
+            //Act           
+            var actual = comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "X");
 
             //Assert
 
-            CollectionAssert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.SequenceEqual(actual));
         }
 
         [TestMethod]
         public void CompareWithYield_NotMatchingStringswithSingleChar_ReturnsNothing()
         {
             //Arrange
-            List<int> expected = new List<int>();
-            //Act
-            Code_Test_Sujish_TextComparer.FmTextComparer comparer = new FmTextComparer();
-            var actual = comparer.CompareWithYield("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Polx");
+            var expected = new List<int>();
+            ICompare comparer = new YieldComparer();
+            //Act         
+            var actual = comparer.Compare("Polly put the kettle on, polly put the kettle on, polly put the kettle on we'll all have tea", "Polx");
 
             //Assert
 
-            CollectionAssert.AreEqual(expected, actual);
+            Assert.IsTrue(expected.SequenceEqual(actual));
         }
 
         #endregion
